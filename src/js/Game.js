ef4517cb68ec4df9ecf3c4f91fb3aaecd76fb26d
Jacob DeBenedetto <deben3@gmail.com>
2025-05-16 08:45:34 -0700

    Refactor interval

--- src/js/Game.js
+++ src/js/Game.js
@@ -1,30 +1,30 @@
 import { Assets } from './Assets.js';
 import { Scene } from './Scene.js';
-import { Ticker } from './Ticker.js';
+import { Interval } from './Interval.js';
 
 class Game {
   constructor() {
     this.assets = new Assets();
     this.scene = new Scene();
-    this.ticker = new Ticker();
+    this.interval = new Interval();
 
     // TODO: Remove test
     this.start();
     console.log(this.scene);
   }
 
-  update({ delta, alpha, frame }) {
-    //console.log(delta, alpha, frame);
+  update(delta, alpha, frame ) {
+    console.log(delta, alpha, frame);
   }
   
-  render({ delta, alpha, frame }) {
-    console.log(alpha);
+  render(delta, alpha, frame) {
+    //console.log(delta, alpha, frame);
   }
 
   start() {
-    this.ticker.add(data => this.update(data), 1000 / 1);
-    this.ticker.add(data => this.render(data), 1000 / 4);
-    this.ticker.start();
+    this.interval.add((delta, alpha, frame) => this.update(delta, alpha, frame), 1000 / 1);
+    this.interval.add((delta, alpha, frame) => this.render(delta, alpha, frame));
+    this.interval.start()
   }
 }
 
