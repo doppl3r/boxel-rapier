1d7ada0cdb77d49d3f1e86ed8926c5ba7f3104e0
Jacob DeBenedetto <deben3@gmail.com>
2026-05-17 08:27:57 -0700

    Update libraries

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import wasm from "vite-plugin-wasm";
-import topLevelAwait from "vite-plugin-top-level-await";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -19,7 +18,6 @@ export default defineConfig({
     } 
   },
   plugins: [
-    topLevelAwait(),
     vue(),
     wasm()
   ],
